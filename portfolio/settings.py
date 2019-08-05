96167f8561359f177159864bb70f5fc95a6b1b61
kovekser <my.ac.kovalenko@gmail.com>
2019-08-05 17:57:20 +0300

    Finished site, added static file on disk

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,7 +124,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'portfolio/static/'),
+    os.path.join(BASE_DIR, 'static'),
+)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
